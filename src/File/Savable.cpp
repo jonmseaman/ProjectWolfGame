9f282ea28461487f3e7fbb7b47ccf1535580aa08
Jon Seaman <jonmseaman@live.com>
2015-12-11 18:25:51 -0500

    Start on implementation for Savable

--- src/File/Savable.cpp
+++ src/File/Savable.cpp
@@ -20,10 +20,11 @@ namespace File {
   fs::fstream file;
   fs::path filePath;
 
-  std::stack<pairType, std::list<pairType>> saveStack;
+  std::stack<pairType*, std::list<pairType*>> saveStack;
 
   /* For storage of information. */
-  boost::property_tree::ptree masterTree;
+  treeType masterTree;
+  treeType* workingTree = nullptr;
 
 
 Savable::Savable()
@@ -36,14 +37,48 @@ Savable::~Savable()
 
 void Savable::start(std::string key)
 {
-  saveStack.push(pairType(key, ptree());
+  saveStack.push(new pairType(key, ptree()));
+  workingTree = &saveStack.top()->second;
 }
 
 void Savable::end()
 {
-  pairType &p = saveStack.top();
+  pairType* p = saveStack.top();
   saveStack.pop();
-  saveStack.top().second.push_back(p);
+  saveStack.top()->second.push_back(*p);
+  workingTree = nullptr;
+}
+
+void Savable::addVariable(const std::string & varName, int var)
+{
+  addVariable(varName, std::to_string(var));
+}
+
+void Savable::addVariable(const std::string & varName, const std::string & var)
+{
+  pairType p{ varName, treeType(var) };
+  workingTree->push_back(p);
+}
+
+void Savable::readVariable(const std::string & varName, int & var)
+{
+  std::string stringValue;
+  readVariable(varName, stringValue);
+
+  // Convert value to int
+  var = std::stoi(stringValue);
+}
+
+void Savable::readVariable(const std::string & varName, std::string & var)
+{
+  // Find var in tree
+
+  // Get the data
+
+  // assign value
+
+  // Clear node in tree
+
 }
 
 } // namespace File
