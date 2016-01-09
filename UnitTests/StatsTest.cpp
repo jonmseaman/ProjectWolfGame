5b7265b18f70e882adeeb958b935b722f1228890
Jon Seaman <jonmseaman@live.com>
2016-01-08 22:24:18 -0500

    Moved some tests to separate project

--- UnitTests/StatsTest.cpp
+++ UnitTests/StatsTest.cpp
@@ -23,5 +23,33 @@ namespace UnitTests
       Assert::AreEqual(0, s.getIntellect());
     }
 
+    TEST_METHOD(additionOperator)
+    {
+      Stats s = Stats{ 1,2,3 } + Stats{ 3,2,1 };
+      Assert::AreEqual(4, s.getStamina());
+      Assert::AreEqual(4, s.getStrength());
+      Assert::AreEqual(4, s.getIntellect());
+    }
+
+    TEST_METHOD(save)
+    {
+      Stats s{ 1,2,3 };
+      for (int i = 0; i < 3; i++) 
+      {
+        s.save();
+      }
+
+      File::save("StatsTest1");
+      // Testing to see if save structure was cleared after save
+      File::save("StatsTest_EmptyFile");
+    }
+
+    TEST_METHOD(File_Load)
+    {
+      // The two files should be identical
+      File::load("StatsTest1");
+      File::save("StatsTest1_Copy");
+    }
+
   };
 }
