7e0cc5a2ef5904180a382c3b4758e865e26fee48
Jon Seaman <jonmseaman@live.com>
2016-05-15 18:34:51 -0400

    More files formatted.

--- Engine/UI/Input.cpp
+++ Engine/UI/Input.cpp
@@ -10,19 +10,32 @@
  * Miscellaneous functions
  */
 
+ /**
+ * Displays a numbered list without a header.
+ * @param listItems The strings that will make up the list.
+ */
 void dispList(const std::vector<std::string> &listItems) {
   for (int i(0); i < listItems.size(); i++) {
-    std::cout << std::setw(COLUMN_PADDING) << std::right << i+1 << ": " << listItems.at(i) << std::endl;
+    std::cout << std::setw(COLUMN_PADDING) << std::right << i + 1 << ": " << listItems.at(i) << std::endl;
   }
 }
-
+/**
+* Displays a numberd list with a header. Stats numbering at 1.
+* @param headText The string that will be the list's header.
+* @param listItems The strings that will make up the list.
+*/
 void dispList(const std::string headText, const std::vector<std::string> &listItems) {
   std::cout << headText << std::endl;
   dispList(listItems);
 }
-
+/**
+* Allows user input of a single digit without requiring the user
+* to press enter.
+* @return int in [min, max]
+* @pre min, max in [0, 9]
+*/
 int getDigit(int min, int max) {
-  assert (min >= 0 && max <= 9);
+  assert(min >= 0 && max <= 9);
   std::string digits = "0123456789";
   int input = _getch();
   int pos = digits.find(char(input));
@@ -41,20 +54,28 @@ char getInput(const std::string &validInput) {
   }
   return char(input);
 }
-
+/**
+* Gets an integer from the user.
+*/
 int getInteger() {
   int input;
-  std::cout <<  " >> ";
+  std::cout << " >> ";
   std::cin >> input;
   while (std::cin.fail()) {
     std::cin.clear();
-    std::cin.ignore(36,'\n');
+    std::cin.ignore(36, '\n');
     std::cout << " >> ";
     std::cin >> input;
   }
   return input;
 }
-
+/**
+* Gets an integer from the user that is between with
+* getInteger in [min, max]
+* @param min the lower bound
+* @param max the upper bound
+* @return getInteger in [min, max]
+*/
 int getInteger(int minVal, int maxVal) {
   int input;
   std::cout << " >> ";
@@ -63,7 +84,7 @@ int getInteger(int minVal, int maxVal) {
     input = getInteger();
     while (std::cin.fail()) {
       std::cin.clear();
-      std::cin.ignore(36,'\n');
+      std::cin.ignore(36, '\n');
     }
     std::cout << " >> ";
     std::cin >> input;
